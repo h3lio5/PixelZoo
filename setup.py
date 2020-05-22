f6cb3e03a02f93af4c0e9276c0c4effb06c262de
Akash SM <akashsm@ce.iitr.ac.in>
2020-05-22 10:23:03 +0530

    minor change

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='pixelzoo',
+      version='1.0',
+      packages=find_packages(),
+      license='GNU GPL V3',
+      author='Akash S M',
+      description=
+      'Implementation of various pixel-based autoregressive generative models')
