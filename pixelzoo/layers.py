ddacd2a0c775d1871586767b2373687562290976
Akash SM <akashsm@ce.iitr.ac.in>
2020-05-22 16:49:48 +0530

    initial commit

---
+++ pixelzoo/layers.py
@@ -0,0 +1,12 @@
+"""
+This file contains various layers used to implement pixel-based autoregressive generative models
+"""
+import torch.nn as nn
+import torch
+
+class MaskedConv2D(nn.Module):
+    """
+
+    """
+
+    def __init__(self,):
