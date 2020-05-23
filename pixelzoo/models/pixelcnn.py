fde5ac560e5d9adfa0659e115e87d9fd89c7a0e9
Akash SM <akashsm@ce.iitr.ac.in>
2020-05-23 23:38:57 +0530

    implementation complete

--- pixelzoo/models/pixelcnn.py
+++ pixelzoo/models/pixelcnn.py
@@ -53,7 +53,7 @@ class PixelCNN(nn.Module):
     """
     Simple PixelCNN model architecture
     """
-    def __init__(self):
+    def __init__(self, num_layers=8, device='cpu'):
 
         super().__init__()
         model = [
@@ -61,7 +61,7 @@ class PixelCNN(nn.Module):
             nn.BatchNorm2d(64),
             nn.ReLU(True)
         ]
-        for _ in range(8):
+        for _ in range(num_layers):
             model.extend([
                 MaskedConv2D('B', 64, 64, 7, padding=3),
                 nn.BatchNorm2d(64),
@@ -69,6 +69,7 @@ class PixelCNN(nn.Module):
             ])
         model.append(MaskedConv2D('B', 64, 1, 7, padding=3, bias=False))
         self.net = nn.Sequential(*model)
+        self.device = device
 
     def nll(self, input):
         """
