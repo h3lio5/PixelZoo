9584f245fea95a30e37486568df5abaff37ade24
h3lio5 <akashsm@ce.iitr.ac.in>
2020-05-26 15:44:26 +0530

    minor change

--- pixelzoo/utils.py
+++ pixelzoo/utils.py
@@ -15,7 +15,7 @@ class EarlyStopping():
     Early stopping callback to speedup convergence and prevent overfitting
     """
 
-    def __init__(self,monitor, patience):
+    def __init__(self, monitor, patience):
         """
         """
 
